3bf54a0b7ada30428f32e1171518e0377b13471c
mengguoru <2506839494@qq.com>
2015-10-22 18:35:20 +0800

    html optimizing

--- WEB/tpl/checknum_pass.php
+++ WEB/tpl/checknum_pass.php
@@ -3,12 +3,10 @@
 	$titleName='验证结果：中国大陆身份证号码计算工具';
 	include_once(PATH.'tpl/header.php');
 ?>
-		<div>根据计算，您输入的身份证号码：<?php echo $inputIDNum; ?>是一个有效的身份证号码。</div>
+		<p>根据计算，您输入的身份证号码：<?php echo $inputIDNum; ?>是一个有效的身份证号码。</p>
 		<div>
-			该身份证发证所在地：<?php echo $placesArr[$placeId];?>。
-			<br />
-			该身份证的持有人出生日期：<?php echo $birthDate[0];?>年<?php echo $birthDate[1];?>月<?php echo $birthDate[2];?>日。
-			<br />
+			该身份证发证所在地：<?php echo $placesArr[$placeId];?>。<br />
+			该身份证的持有人出生日期：<?php echo $birthDate[0];?>年<?php echo $birthDate[1];?>月<?php echo $birthDate[2];?>日。<br />
 			该身份证的持有人是<?php echo $sex;?>性。
 		</div>
 		<div>
@@ -16,3 +14,4 @@
 		</div>
 <?php
 include_once(PATH.'tpl/footer.php');
+?>
