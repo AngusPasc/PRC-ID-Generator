8491b4d15785b7ae969e3521f7d2589fda2cab4b
Enex Slurin <Enex Slurin>
2016-02-21 18:50:41 +0800

    (PHP) 地区编码页面翻页修正 现在，不管当前页码是多少，翻页导航总是生成7个翻页链接。

--- WEB/tpl/showplacesnum.php
+++ WEB/tpl/showplacesnum.php
@@ -12,14 +12,16 @@ foreach($showArr as $k=>$v){echo "\t\t\t{$k}-{$v}\n\t\t\t<br />\n";}
 			<form action="<?php echo SURL;?>" method="post">
 				<?php
 				  # 翻页导航生成部分
-					$pagerAmount=3;
-					$pagerMinNumber=$currentPage-$pagerAmount<1?1:$currentPage-$pagerAmount;
-					$pagerMaxNumber=$currentPage+$pagerAmount>$maxPageNum?$maxPageNum:$currentPage+$pagerAmount;
+					$pagerAmount = 3;
+					$pagerMinNumber = $currentPage + $pagerAmount > $maxPageNum ? $maxPageNum - 1 - 2 * $pagerAmount : $currentPage - $pagerAmount;
+					$pagerMinNumber = $pagerMinNumber < 1 ? 1 : $pagerMinNumber;
+					$pagerMaxNumber = $currentPage - $pagerAmount < 1 ? 2 * $pagerAmount + 1 : $currentPage + $pagerAmount;
+					$pagerMaxNumber = $pagerMaxNumber > $maxPageNum ? $maxPageNum : $pagerMaxNumber;
 					$pageRes='';
-					for($i=$pagerMinNumber;$i<=$pagerMaxNumber;$i++){ ?>
-						<a href="<?php echo SURL;?>?act=showplacesnum&amp;p=<?php echo $i; ?>"><?php echo $i; ?></a>&nbsp;
-					<?php } ?>
-				<?php echo $pageRes;?>
+					for($i = $pagerMinNumber; $i <= $pagerMaxNumber; $i++){
+						if($i == $currentPage){echo $i, '&nbsp;'; continue; }
+						?><a href="<?php echo SURL;?>?act=showplacesnum&amp;p=<?php echo $i; ?>"><?php echo $i; ?></a>&nbsp;
+					<?php } echo $pageRes; ?>
 				<br />
 				当前第<?php echo $currentPage;?>页，共<?php echo $maxPageNum;?>页。
 				<br />
